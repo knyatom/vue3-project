9337ac978f859a6053affcbb4a801da9d6a832f2
knyatom <58832580+knyatom@users.noreply.github.com>
2021-12-04 01:23:03 +0900

    12-04

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -5,21 +5,29 @@
     {{ greet }}
     <button class="btn btn-primary" v-on:click="consoleLog">Click</button>
     <button class="btn btn-primary" v-on:click="updateName">Update</button>
-
   </div>
 </template>
 
 <script>
 export default {
   setup() {
-    const name = "kim NY";
+    let name = "kim NY";
     const greeting = (name) => {
       return "Hello " + name;
     };
     const greet = greeting(name);
+    const consoleLog = () => {
+      console.log("Hello World");
+    };
+    const updateName = () => {
+      name = "Kim Yu Na";
+      console.log(name);
+    };
     return {
       name,
       greet,
+      consoleLog,
+      updateName
     };
   },
 };
