562486eaca95c0cf649f22d5d406fd48780d1546
knyatom <58832580+knyatom@users.noreply.github.com>
2021-12-05 12:20:31 +0900

    1205

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,45 +1,25 @@
 <template>
-  <div v-bind:class="nameClass">
-    <div class="name">{{ name }}- {{ name2 }}</div>
-    <div>HI. Welcome</div>
-    {{ greet }}
-    <button class="btn btn-primary" v-on:click="consoleLog">Click</button>
-    <button class="btn btn-primary" v-on:click="updateName">Update</button>
-    <br />
-    <input type="text" v-bind:value="name" />
-    <input v-bind:type="type" v-bind:value="name" />
+  <div :class="nameClass">
+    <input type="text" v-model="name" />
+    <button class="btn btn-primary" @click="onSubmit">Click</button>
+    <p>{{name}}</p>
   </div>
 </template>
 <script>
 import { ref } from "vue";
-import { reactive } from "vue";
 
 export default {
   setup() {
-    const name = ref("Kim NY Welcome");
-    const name2 = reactive({ id: 1 });
-    const type = ref("text");
-    const nameClass = ref("name");
-    const consoleLog = () => {
-      console.log("Hello World");
-      name.value = "Hello";
-      name2.id = "KIM";
-       nameClass.value = "name";
-    };
+    const name = ref("Mike Jackson");    
+    const nameClass=ref("nameClass");
 
-    const updateName = () => {
-      name.value = "Kim Yu Na";
-      name2.id = "Welcome";
-      type.value = "number";
-      nameClass.value = "name2";
+    const onSubmit = () => {
       console.log(name.value);
     };
+    
     return {
       name,
-      consoleLog,
-      updateName,
-      name2,
-      type,
+      onSubmit,    
       nameClass
     };
   },
@@ -52,5 +32,8 @@ export default {
 }
 .name2 {
   color: red;
+}
+.nameClass{
+  color:'gray';
 }
 </style>
