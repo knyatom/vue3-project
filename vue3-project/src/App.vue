03a52f4ace5aa537b3bbd701a084c4af1f0f0a2d
knyatom <58832580+knyatom@users.noreply.github.com>
2021-12-06 23:11:46 +0900

    1206

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,39 +1,38 @@
 <template>
-  <div :class="nameClass">
-    <input type="text" v-model="name" />
-    <button class="btn btn-primary" @click="onSubmit">Click</button>
-    <p>{{name}}</p>
+  <div class="container">
+    <h1> To-do List</h1>
+    <form class="d-flex">
+      <div class="flex-grow-1 mr-2">
+       <input type="text" class="form-control" v-model="todo" placeholder="Type Text">
+      </div> 
+      <button class="btn btn-primary" @click="onSubmit">
+        Add
+      </button>
+    </form>
+    {{todos}}
   </div>
 </template>
 <script>
 import { ref } from "vue";
-
 export default {
-  setup() {
-    const name = ref("Mike Jackson");    
-    const nameClass=ref("nameClass");
-
-    const onSubmit = () => {
-      console.log(name.value);
-    };
-    
+  setup(){
+   const todo=ref('');
+   const todos=ref([]);
+   const onSubmit=(e)=>{
+     e.preventDefault();
+     console.log(todo.value);
+     todos.value.push({
+       id:Date.now(),
+       subject:todo.value
+     })
+   }   
     return {
-      name,
-      onSubmit,    
-      nameClass
-    };
-  },
+      todo,todos,onSubmit
+     }
+  }
 };
 </script>
 
 <style>
-.name {
-  color: blue;
-}
-.name2 {
-  color: red;
-}
-.nameClass{
-  color:'gray';
-}
+
 </style>
